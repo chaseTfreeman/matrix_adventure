f2632718bee53b2c054a428ff0434ba03a73fadc
Chase <chasefreeman13@gmail.com>
2016-07-01 17:28:06 -0400

    Created Intro

---
+++ script.js
@@ -0,0 +1,26 @@
+// var choices = [1, 2, 3, 4, 5, 6];
+//maybe change "choices" to "Prompts" and the oucomes of prompts are the parameteres for moving on
+
+// var bluePill = false;
+// var redPill = true;
+
+var ready = prompt("Wake up, Neo. Are you ready?");
+if (ready === "yes") {
+  console.log("Great. Follow the white rabbit");
+}
+else {
+  console.log("..but you have to be, Neo. You are the One. The Matrix has you. Follow the white rabbit. 'Knock, Knock.'")
+
+}
+
+var questionsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+ questionsArray[1] = prompt("Morpheus: Welcome, Neo. Do you want to know what the Matrix is?");
+ for (i = 10; i > 1; i -= 1) {
+}
+
+// var choices = prompt("Red Pill or Blue Pill?")
+//  if (choice1 = false) {
+//    for (var i = chocies;i choices - 1);
+//  }
+// console.log(choices[1]);
