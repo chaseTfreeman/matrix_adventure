27d95d40bd4df9bedcb56abbfb35ad9155861f74
Chase <chasefreeman13@gmail.com>
2016-07-04 21:06:23 -0400

    pushing what I've got

--- script.js
+++ script.js
@@ -4,23 +4,50 @@
 // var bluePill = false;
 // var redPill = true;
 
+
+
 var ready = prompt("Wake up, Neo. Are you ready?");
-if (ready === "yes") {
-  console.log("Great. Follow the white rabbit");
-}
-else {
-  console.log("..but you have to be, Neo. You are the One. The Matrix has you. Follow the white rabbit. 'Knock, Knock.'")
 
-}
+ if (ready != null) {
+    confirm("Great. Follow the white rabbit");
 
-var questionsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  }
+  else {
+  confirm("..but you have to be, Neo. You are the One. The Matrix has you. Follow the white rabbit. 'Knock, Knock")
+};
 
- questionsArray[1] = prompt("Morpheus: Welcome, Neo. Do you want to know what the Matrix is?");
- for (i = 10; i > 1; i -= 1) {
-}
+  var meetMorpheus = prompt("After a bit of trouble, you meet Morpheus." + " " + "Morpheus says: 'Welcome, Neo. Do you know who I am?''");
 
-// var choices = prompt("Red Pill or Blue Pill?")
-//  if (choice1 = false) {
-//    for (var i = chocies;i choices - 1);
-//  }
-// console.log(choices[1]);
+    if( meetMorpheus != null || "no" || "No") {
+      confirm("Great. That makes this easier.")
+    }
+
+    else {
+      confirm("In the Matrix and to the Agents I am an international criminal wanted for almost every cyber-crime in the books, if not more. However, I am not those things to you. I can be a trusted friend. I am a commander from Zion--more on that later.")
+    };
+
+var trust = prompt("Do you trust me? [type Yes or No]");
+
+   if (trust === "Yes" || "yes") {
+     confirm("Excellent. Lets Continue.")
+   }
+
+  else {
+    confirm("Just listen to what I have to say.")
+  }
+
+  var searchYears = parseFloat(prompt("Morpheus says 'how many years have you been looking for the Matrix?'"));
+
+
+    for (searchYears; searchYears < 5; searchYears++) {
+        alert(searchYears + "years?" + "I've been looking for you my whole life!")
+        }
+
+        if  (seachYears > 6) {
+  alert("We've been looking for you longer.");
+  }
+  else {
+   alert("So be it, we've been looking for you for a VERY long time. ");
+ }
+
+var matrixIntro = prompt("Morpheus says: 'Lets continue. You have some choices to make'");
